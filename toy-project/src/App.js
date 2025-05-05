fef9a30a8aceeb69ba26b0d132bde6b2c53d8088
e-unhee <dkfzhd1509@g.skku.edu>
2025-05-06 03:32:20 +0900

    프론트 1차 작업(상단 배너)

--- toy-project/src/App.js
+++ toy-project/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className='navbar'>
+        <div style={{'color':'white'}}>
+          초짜도 같이 가챠</div>
+        <nav>
+          <ul className='nav-ul'>
+            <li className='nav-item' style={{'color':'#DF6565'}}>짱구가챠</li>
+            <li className='nav-item' style={{'color':'#F6A2C0'}}>산리오가챠</li>
+            <li className='nav-item' style={{'color':'#58BFB6'}}>애니가챠</li>
+            <li className='nav-item' style={{'color':'#794DBB'}}>음식가챠</li>
+            <button className='nav-but'>log in</button>
+          </ul>
+        </nav>
       </header>
     </div>
   );
